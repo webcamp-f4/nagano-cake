74821c9c2ade1a14d6532c38a67d4cf8af41f4b5
vagrant <vagrant@localhost.localdomain>
2020-07-02 09:24:39 +0000

    [Add]新規登録画面＆ログイン画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+	# ログイン済ユーザーのみにアクセスを許可する
+	before_action :authenticate_customer!, except: [:top, :about]
+	# deviseコントローラーにストロングパラメータを追加する
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	protect_from_forgery with: :exception
+
+	def after_sign_in_path_for(resource)
+    	public_customer_path(resource)
+  	end
+
+  	def after_sign_out_path_for(resource)
+		root_path(resource)
+	end
+
+	protected
+		def configure_permitted_parameters
+			# サインアップ時にストロングパラメータを追加
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana, :first_name_kana, :address, :last_name_kanji, :first_name_kanji, :postal_code, :tel])
+		end
 end
