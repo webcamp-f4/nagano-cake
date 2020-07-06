5dfc538aedd62a98c2d7adfea407b79169f6f5b7
vagrant <vagrant@localhost.localdomain>
2020-07-06 04:25:40 +0000

    [Fix]ショッピングカート画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,22 @@
 class Public::CartItemsController < ApplicationController
 
 	def index
-		@customer = current_customer
-		@cart_items = @customer.cart_items
+		@cart_items = CartItem.all
+		@cart_item = CartItem.new
+	end
+
+	def create
+		@cart_item = current_customer.cart_items.build(cart_item_params)
+		@cart_items = current_customer.cart_items.all
+		@cart_items.each do |cart_item|
+			if cart_item.item_id == @cart_item.item_id
+				new_amount = cart_item.amount + @cart_item.amount
+				cart_item.update_attribute(:amount, new_amount)
+				@cart_item.delete
+			end
+		end
+		@cart_item.save
+		redirect_to request.referer
 	end
 
 	def update
