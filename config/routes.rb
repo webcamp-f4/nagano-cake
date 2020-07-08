840b36910a9d28f9a643fec4dc8aa339f081826e
vagrant <vagrant@localhost.localdomain>
2020-07-08 04:22:59 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
 
   namespace :public do
   	resources :cart_items, only: [:index, :create, :update, :destroy]
-    delete "cart_items/destroy_all" => "cart_items#destroy_all"
   	resources :items, only: [:index, :show]
   	resources :orders, only: [:new, :create]
     post '/orders/create_shipping' => 'orders#create_shipping' #情報入力画面での配送先登録用のアクション
