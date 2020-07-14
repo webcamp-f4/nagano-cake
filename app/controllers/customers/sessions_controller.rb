6c1bc9473d68498c2238dbba896d754ed7f29f94
vagrant <vagrant@localhost.localdomain>
2020-07-14 03:59:20 +0000

    [Fix]新規登録機能とログイン機能

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -24,15 +24,9 @@ class Customers::SessionsController < Devise::SessionsController
     respond_to_on_destroy
   end
 
-
-
-
-
-
 protected
   def configure_permitted_parameters
 			# サインアップ時にストロングパラメータを追加
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana, :first_name_kana, :address, :last_name_kanji, :first_name_kanji, :postal_code, :tel, :admin])
   end
-
 end
